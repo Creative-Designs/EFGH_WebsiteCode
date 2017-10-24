a57dfe56074b1ea93d303bacba346fde86874eee
CreativeFingers <30295395+CreativeFingers@users.noreply.github.com>
2017-10-24 22:18:17 +0530

    EFGH
    EFGH website

---
+++ style.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+//========================navigate smoothly============
+	$("a").click(function(event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+		  // Prevent default anchor click behavior
+		  event.preventDefault();
+
+		  // Store hash
+		  var hash = this.hash;
+
+		  // Using jQuery's animate() method to add smooth page scroll
+		  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+	   
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  });
+		} // End if
+  });
+  
+    // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+ //top btn
+  $(window).scroll(function(){
+	  
+	  if
+		  ($(this).scrollTop()>400){
+			  $("#top_btn").fadeIn();
+			  
+		  }
+		else{
+			$("#top_btn").fadeOut();
+			
+		}
+	  
+	  
+	  
+	  
+  });
+	$("#top_btn").click(function(){
+		$("body,html").animate({scrollTop:0},1000);
+	});
+  
+});
